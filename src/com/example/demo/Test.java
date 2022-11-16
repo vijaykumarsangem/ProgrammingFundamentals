081887c95945e284578a838cdda98ada5e96bd50
vijay <sangemvijay97@gmail.com>
2022-11-16 11:49:44 +0530

    created a new class and main method

---
+++ src/com/example/demo/Test.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int[] arr = new int[4];
+		for (int i = 0; i < arr.length; i++)
+			System.out.println("Element at index " + i + " : " + arr[i]);
+		int[] array= {3,3,3,3};
+		for (int i = 0; i < array.length; i++)
+			System.out.println("Element at index " + i + " : " + array[i]);
+		int[] array1 = {2,2,2,2,2,2,2};
+	}
+
+}
